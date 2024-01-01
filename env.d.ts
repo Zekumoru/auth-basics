5d0913043f7b3efc4637453f2e41adaf954a614b
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-01-01 11:21:40 +0100

    Initial commit

---
+++ env.d.ts
@@ -0,0 +1,13 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      HOSTNAME?: string;
+      HOST?: string;
+      PORT?: string;
+      MONGODB_CONNECT_STRING?: string;
+      NODE_ENV?: 'development' | 'production';
+    }
+  }
+}
+
+export { };
